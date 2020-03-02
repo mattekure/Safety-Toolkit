76ed7512ee0cb05b9ef46334a15b23cecc8924e1
mattekure <mattekure@gmail.com>
2020-03-01 20:51:52 -0500

    Initial attempts at adding survey

--- extension/scripts/manager_safetytk.lua
+++ extension/scripts/manager_safetytk.lua
@@ -4,13 +4,75 @@ local unAck = {
 	o=0
 }
 
+local topicList = {
+	"Child abuse",
+	"Clowns",
+	"Drugs",
+	"High elevations (flight, high-rises, etc)",
+	"Sexual abuse",
+	"Slime and pus",
+	"Spiders",
+	"Suicide",
+	"Torture",
+	"Violence against animals",
+	"Violence against children"
+}
+
 OOB_MSGTYPE_SUBMITCARD = "submitcard";
 
 function onInit()
-		Interface.onDesktopInit = onDesktopInit;
-		OOBManager.registerOOBMsgHandler(OOB_MSGTYPE_SUBMITCARD, handleSubmitCard);
+	Interface.onDesktopInit = onDesktopInit;
+	OOBManager.registerOOBMsgHandler(OOB_MSGTYPE_SUBMITCARD, handleSubmitCard);
+	local nNode = DB.findNode("safetyTopics");
+	local mNode = "";
+	local lNode = "";
+	if nNode == nil then
+		nNode = DB.createNode("safetyTopics");
+		for _,v in pairs(topicList) do
+			local mNode = DB.createChild(nNode)
+			local lNode = mNode.createChild("name", "string");
+			lNode.setValue(v);
+			lNode = mNode.createChild("responseNum", "number");
+			lNode.setValue(0);
+		end		
+	end
+		
 end
 
+function getTopics()
+	local nNode = DB.findNode("safetyTopics");
+	local sTopicList = "";
+	local tNode = "";
+	if nNode ~= nil then
+		nodeTable = nNode.getChildren();
+		for i,v in pairs(nodeTable) do
+			tNode = v.getChild("name");
+			sTopicList = sTopicList .. tNode.getValue() .. string.char(10);
+		end
+	end 
+	return StringManager.trim(sTopicList);
+end
+
+function setTopics(sTopics)
+	local nNode = DB.findNode("safetyTopics");
+	local mNode = "";
+	local lNode = "";
+	local nTable = nNode.getChildren()
+	for i,v in pairs(nTable) do
+		DB.deleteNode(v);
+	end
+	
+	for sTopic in string.gmatch(sTopics, '([^\n]+)') do
+		sTopic = StringManager.trim(sTopic);
+		local mNode = nNode.createChild()
+		local lNode = mNode.createChild("name", "string");
+		lNode.setValue(sTopic);
+		lNode = mNode.createChild("responseNum", "number");
+		lNode.setValue(0);
+		
+	end
+
+end
 -- ---------------------------------------------------------------
 -- OnDesktopInit - Called right after Desktop is fully loaded	--
 -- ---------------------------------------------------------------
@@ -23,6 +85,8 @@ end
 function RegisterStackShortcut()
 	if DesktopManager ~= nil then
 		DesktopManager.registerStackShortcut2("safety_toolkit", "safety_toolkit_down", "safetyToolkit_tooltip", "SafetyToolkitWindow", "", true);
+		DesktopManager.registerStackShortcut2("triggers", "triggers_down", "triggersSurvey", "triggersWindow", "", true);
+
 	end
 end
 
